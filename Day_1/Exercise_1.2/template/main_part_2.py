9745bd248da057a0c4127a585a95e209b360010e
Marco Vaccari <>
2024-09-05 10:58:18 +0200

    Update main_part_2.py

--- Day_1/Exercise_1.2/template/main_part_2.py
+++ Day_1/Exercise_1.2/template/main_part_2.py
@@ -24,35 +24,6 @@ class CSTRParameters:
     xs: np.ndarray = np.array([0.878, 324.5, 0.659])
     us: np.ndarray = np.array([300, 0.1])
 
-
-@dataclass
-class MpcCstrParameters:
-    umin: np.ndarray  # lower bound on u
-    umax: np.ndarray  # upper bound on u
-    Q: np.ndarray
-    R: np.ndarray
-    Tf: float = 0.25 * 15  # horizon length
-    N: int = 20
-    dt: float = 0.25
-    num_rk4_steps: int = 10
-
-    # NOTE: computed with setup_linearized_model()
-    P: np.ndarray = np.array(
-        [
-            [5.92981953e-01, -8.40033347e-04, -1.54536980e-02],
-            [-8.40033347e-04, 7.75225208e-06, 2.30677411e-05],
-            [-1.54536980e-02, 2.30677411e-05, 2.59450075e00],
-        ]
-    )
-
-    def __init__(self, xs, us):
-        self.Q = np.diag(1.0 / xs**2)
-        self.R = np.diag(1.0 / us**2)
-
-        self.umin = np.array([0.95, 0.85]) * us
-        self.umax = np.array([1.05, 1.15]) * us
-
-
 def setup_cstr_model(dt: float, num_steps: int, params: CSTRParameters):
 
     # set up states
@@ -101,11 +72,6 @@ def main_mpc_open_loop():
     us = params.us
     N_horizon = 20
 
-    # TODO setup cost expressions
-    # Use model.x_expr and model.u_expr to set up the stage and terminal cost of the OCP
-    # the references are given by xs and us
-    # the weighting matrices are given by Q, R, and P
-    
     # NOTE: computed with setup_linearized_model()
     P = np.array(
         [
@@ -118,13 +84,15 @@ def main_mpc_open_loop():
     Q = np.diag(1.0 / xs**2)
     R = np.diag(1.0 / us**2)
 
-    delta_x = model.x_expr - model.x_steady_state
-    delta_u = model.u_expr - model.u_steady_state
-    stage_cost = 0.5 * delta_x.T @ Q @ delta_x + 0.5 * delta_u.T @ R @ delta_u
-    terminal_cost = 0.5 * delta_x.T @ P @ delta_x
+    # TODO setup cost expressions
+    # Use model.x_expr and model.u_expr to set up the stage and terminal cost of the OCP
+    # the references are given by xs and us
+    # the weighting matrices are given by Q, R, and P
+    stage_cost = 42
+    terminal_cost = 42
 
-    umin = None #np.array([0.95, 0.85]) * us
-    umax = None #np.array([1.05, 1.15]) * us
+    umin = np.array([0.95, 0.85]) * us
+    umax = np.array([1.05, 1.15]) * us
 
     ocp_solver = OCPsolver(model, stage_cost, terminal_cost, N_horizon, umax, umin)
 
@@ -132,12 +100,12 @@ def main_mpc_open_loop():
     u_ref  = np.array([model.u_steady_state]*N_horizon).T
 
     # we use umin as initial guess
-    u_init  = np.array([mpc_params.umin]*N_horizon).T
+    u_init  = np.array([umin]*N_horizon).T
     (u_traj, cost) = ocp_solver.solve(x0, u_traj_init=u_init)
 
     x_traj = model.simulate_traj(x0, u_traj)
 
-    plot_cstr(mpc_params.dt, [x_traj], [u_traj], x_ref, u_ref, mpc_params.umin, mpc_params.umax, ['open-loop solution'])
+    plot_cstr(dt, [x_traj], [u_traj], x_ref, u_ref, umin, umax, ['open-loop solution'])
 
 
 
